09af4fbcf67a7e753fc1dfd3463f3f7903a8ff5b
Estebandidoo <56638674+Estebandidoo@users.noreply.github.com>
2019-11-18 16:31:10 -0300

    Se corrige url de template

--- pañol/urls.py
+++ pañol/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import include
 from django.conf.urls import url
-
+ 
 urlpatterns = [
   path('',views.index,name='index'),
   path('index/',views.index,name='index'),
